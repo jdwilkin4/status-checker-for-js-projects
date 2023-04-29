19b2ca55a26475eb5345a2b2cfe2d3ce56db955d
jdwilkin4 <jwilkin4@hotmail.com>
2023-04-29 16:27:24 -0700

    feat: adding media query for list items

--- src/components/ProjectTemplate.tsx
+++ src/components/ProjectTemplate.tsx
@@ -2,23 +2,24 @@ import { CheckIcon } from "@chakra-ui/icons";
 import {
   Heading,
   Box,
-  Link,
-  Text,
   List,
   ListItem,
   ListIcon,
   Grid,
+  useMediaQuery,
 } from "@chakra-ui/react";
 import { ConceptsArr } from "../pages/types";
-import NextLink from "next/link";
+
 import { HomeLink } from "./HomeLink";
 
 type Props = {
-  title: string;
+  title?: string;
   conceptsArr: ConceptsArr;
 };
 
 export const ProjectTemplate = ({ title, conceptsArr }: Props) => {
+  const [isTabletSizeOrLarger] = useMediaQuery("(min-width: 768px)");
+
   return (
     <Box textAlign="center" color="white" bg="grey">
       <Heading role="heading" as="h1">
@@ -30,17 +31,29 @@ export const ProjectTemplate = ({ title, conceptsArr }: Props) => {
       </Heading>
 
       <List my={8} spacing={3}>
-        <Grid templateColumns="repeat(2, 1fr)" gap={6}>
-          {conceptsArr.map((concept, index) => (
-            <ListItem key={`${concept}-${index}`} fontSize="2xl">
-              <ListIcon as={CheckIcon} color="green.500" />
-              {concept}
-            </ListItem>
-          ))}
-        </Grid>
+        {isTabletSizeOrLarger ? (
+          <Grid templateColumns="repeat(2, 1fr)" gap={6}>
+            <ListItems conceptsArr={conceptsArr} />
+          </Grid>
+        ) : (
+          <ListItems conceptsArr={conceptsArr} />
+        )}
       </List>
 
       <HomeLink />
     </Box>
+  );
+};
+
+const ListItems = ({ conceptsArr }: Props) => {
+  return (
+    <>
+      {conceptsArr.map((concept, index) => (
+        <ListItem key={`${concept}-${index}`} fontSize="2xl">
+          <ListIcon as={CheckIcon} color="green.500" />
+          {concept}
+        </ListItem>
+      ))}
+    </>
   );
 };
